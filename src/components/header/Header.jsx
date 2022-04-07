defe2c0d673dd4b9102ca88e173439753dd0ae16
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2022-04-07 10:27:38 -1100

    Settings in mobile first

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,14 @@
-import s from './header.module.css';
+import s from './Header.module.css';
+import logoImg from '../../image/logo.png';
 
 const Header = () => {
     return (
         <>
             <header className={s.header}>
                 <div className={s.container}>
-                    <img src='../../image/logo.jpg' alt='logo' className={s.logo} />
+                    <img src={logoImg} alt='logo' />
                     <div className={s.navigation}>
-                        <a href='№' className={s.contacts}>Контакты</a>
+                        <a href='#' className={s.contacts}>Контакты</a>
                         <button className={s.login}>Войти</button>
                     </div>
                 </div>
